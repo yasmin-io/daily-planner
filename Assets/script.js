81e0f358369ec1985475496acf483bf54e9b585b
Yasmin <yasmin.bunnie@gmail.com>
2022-01-20 18:18:33 +0000

    timer intervals to change the color according to time of day

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,5 @@
+// having everything inside this function helps everything on
+//the page load together
 $(document).ready(function () {
   $(".btn").on("click", function () {
     var activity = $(this).parent().siblings(".description").val();
@@ -7,9 +9,37 @@ $(document).ready(function () {
     localStorage.setItem(timeId, activity);
   });
 
+  function timeCheck() {
+    //use moment to get the current hour
+    var currentHour = moment().hours();
+
+    $(".input-group").each(function () {
+      //we iterate over every block of time - each block is 'this' when it is its turn to have the function ran on it
+      var inputGroupId = $(this).attr("id");
+      var timeForInputGroup = parseInt(inputGroupId);
+
+      if (timeForInputGroup < currentHour) {
+        $(this)
+          .children(".description")
+          .addClass("bg-info text-white")
+          .attr("style", "--bs-bg-opacity: .5;");
+      } else if (timeForInputGroup === currentHour) {
+        //add styling for the present hour
+      } else {
+        //add styling for future
+      }
+    });
+  }
+
+  //run check on hours vs current hour before loading from local storage
+
+  timeCheck();
   //Get values from local storage and add to page when loads
   $("#9 .description").val(localStorage.getItem("9"));
   $("#10 .description").val(localStorage.getItem("10"));
+
+  //set interval for modifying past, present and future classes on elements
+  var interval = setInterval(timeCheck, 30000);
 
   // Using moment.js for time/date instead of native javascript
   var today = moment();
